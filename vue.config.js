cdbbda544c1a7654f68f5d92edacfd6810f08970
zhoushiping <17300792864@163.com>
2021-02-21 09:32:11 +0800

    feat: update

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
 module.exports = {
-    configureWebpack: {
-
+  configureWebpack: {
+    // 配置路径相关的时候用resolve 可以解决路径相关的问题
+    resolve: {
+      alias: {
+        // 默认有@  他指向的是：src
+        assets: '@/assets',
+        components: '@/components'
+      }
     }
+  }
 }
