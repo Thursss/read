d0051f2b71ed729164628be81195aa06cb3b07f3
Thursss <1720557879@qq.com>
2021-03-15 14:50:51 +0800

    测试 commitlint

--- vue.config.js
+++ vue.config.js
@@ -14,11 +14,7 @@ module.exports = {
         utils: '@/utils'
       }
     },
-    husky: {
-      hooks: {
-        'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS'
-      }
-    },
+
   },
   css: {
     loaderOptions: {
